4d456093a2fff00f3941e6c42248660f565cd788
Quinten Clause <quinten.clause@studioemma.eu>
2016-09-06 18:25:49 +0200

    Hack for prompt not working in web app mode

--- pmanifest.php
+++ pmanifest.php
@@ -1,7 +1,7 @@
 <?php
 header("Content-Type: text/cache-manifest;");
 echo "CACHE MANIFEST\r\n";
-echo "# v25\r\n"; // if you make an adjustment to your index.html be sure to advance the versionnumber or make an other adjustment to the manifest so the cache will be cleared
+echo "# v26\r\n"; // if you make an adjustment to your index.html be sure to advance the versionnumber or make an other adjustment to the manifest so the cache will be cleared
 echo "CACHE:\r\n";
 echo "jquery-1.5.min.js\r\n";
 echo "processing-1.0.0.min.js\r\n";
